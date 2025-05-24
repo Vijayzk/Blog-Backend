c8ffac0c8e69f74f2b9fb583072933ae0e703c76
Vijay Anand <va776683@gmail.com>
2025-05-24 23:24:49 +0530

    Update db.config.js

--- DB/db.config.js
+++ DB/db.config.js
@@ -1,7 +1,8 @@
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient({
-    log:["query"], //later comment
+    log: ['error', 'warn'],
+    //log:["query"], //later comment
 })
 
 export default prisma;
